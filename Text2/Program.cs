58e3d1fb815a1814a0b301f87d8059c5d2cce9da
liorshl <lior.shlimenzon@gmail.com>
2025-05-27 20:57:50 +0300

    bjer

--- Text2/Program.cs
+++ Text2/Program.cs
@@ -10,197 +10,197 @@ using ViewModel;
 
 
 
-Kind kc = KindDb.SelectById(1);
-KindDb kcdb = new KindDb();
-Kindlist kcs = kcdb.SelectAll();
-Console.WriteLine("students list before :");
-foreach (Kind c in kcs)
-    Console.WriteLine(c.kind);
-//Kind kLast = kcs.Last();
-//kcdb.Delete(kLast);
-//kcdb.SaveChanges();
-Console.WriteLine("delete:");
-kcs = kcdb.SelectAll();
-foreach (Kind c in kcs)
-    Console.WriteLine(c.kind);
-Kind ky = new Kind() { kind = "luxury" };
-KindDb kyd = new KindDb();
-kyd.Insert(ky);
-kyd.SaveChanges();
-Kindlist kinds = kyd.SelectAll();
-foreach (Kind c in kinds)
-{
-    Console.WriteLine(c.kind);
-}
-Kind p2 = kinds[2];
-p2.kind = "electric";
-kyd.Update(p2);
-kyd.SaveChanges();
-Kindlist kinds1 = kyd.SelectAll();
-foreach (Kind c in kinds1)
-{
-    Console.WriteLine(c.kind);
-}
+//Kind kc = KindDb.SelectById(1);
+//KindDb kcdb = new KindDb();
+//Kindlist kcs = kcdb.SelectAll();
+//Console.WriteLine("students list before :");
+//foreach (Kind c in kcs)
+//    Console.WriteLine(c.kind);
+////Kind kLast = kcs.Last();
+////kcdb.Delete(kLast);
+////kcdb.SaveChanges();
+//Console.WriteLine("delete:");
+//kcs = kcdb.SelectAll();
+//foreach (Kind c in kcs)
+//    Console.WriteLine(c.kind);
+//Kind ky = new Kind() { kind = "luxury" };
+//KindDb kyd = new KindDb();
+//kyd.Insert(ky);
+//kyd.SaveChanges();
+//Kindlist kinds = kyd.SelectAll();
+//foreach (Kind c in kinds)
+//{
+//    Console.WriteLine(c.kind);
+//}
+//Kind p2 = kinds[2];
+//p2.kind = "electric";
+//kyd.Update(p2);
+//kyd.SaveChanges();
+//Kindlist kinds1 = kyd.SelectAll();
+//foreach (Kind c in kinds1)
+//{
+//    Console.WriteLine(c.kind);
+//}
 
 
-#endregion
-#region
-Brand brc = BrandDb.SelectById(1);
-BrandDb brdb = new BrandDb();
-Brandlist brs = brdb.SelectAll();
-Console.WriteLine("students list before :");
-foreach (Brand c in brs)
-    Console.WriteLine(c.worth);
-Brand brLast = brs.Last();
-//brdb.Delete(brLast);
-//brdb.SaveChanges();
-Console.WriteLine("delete:");
-brs = brdb.SelectAll();
-foreach (Brand c in brs)
-    Console.WriteLine(c.worth);
-Brand bry = new Brand() { worth = 4440, Bname = "suzuki", numcars = 3 };
-BrandDb brd = new BrandDb();
-//brd.Insert(bry);
+//#endregion
+//#region
+//Brand brc = BrandDb.SelectById(1);
+//BrandDb brdb = new BrandDb();
+//Brandlist brs = brdb.SelectAll();
+//Console.WriteLine("students list before :");
+//foreach (Brand c in brs)
+//    Console.WriteLine(c.worth);
+//Brand brLast = brs.Last();
+////brdb.Delete(brLast);
+////brdb.SaveChanges();
+//Console.WriteLine("delete:");
+//brs = brdb.SelectAll();
+//foreach (Brand c in brs)
+//    Console.WriteLine(c.worth);
+//Brand bry = new Brand() { worth = 4440, Bname = "suzuki", numcars = 3 };
+//BrandDb brd = new BrandDb();
+////brd.Insert(bry);
+////brd.SaveChanges();
+//Brandlist brands = brd.SelectAll();
+//foreach (Brand c in brands)
+//{
+//    Console.WriteLine(c.worth);
+//}
+//Brand b2 = brands[2];
+//b2.Bname = "kia";
+//brd.Update(b2);
 //brd.SaveChanges();
-Brandlist brands = brd.SelectAll();
-foreach (Brand c in brands)
-{
-    Console.WriteLine(c.worth);
-}
-Brand b2 = brands[2];
-b2.Bname = "kia";
-brd.Update(b2);
-brd.SaveChanges();
-Brandlist brands1 = brd.SelectAll();
-foreach (Brand c in brands1)
-{
-    Console.WriteLine(c.Bname);
-}
+//Brandlist brands1 = brd.SelectAll();
+//foreach (Brand c in brands1)
+//{
+//    Console.WriteLine(c.Bname);
+//}
 
-#endregion
-#region
-Seller sc = sellerDb.SelectById(1);
-sellerDb sDB = new sellerDb();
-Sellerlist sts = sDB.SelectAll();
-Console.WriteLine("students list before :");
-foreach (Seller c in sts)
-    Console.WriteLine(c.lname);
-Seller sLast = sts.Last();
-//sDB.Delete(sLast);
-//sDB.SaveChanges();
-Console.WriteLine("delete:");
-sts = sDB.SelectAll();
-foreach (Seller c in sts)
-    Console.WriteLine(c.lname);
-#endregion
-#region
-City1 c1c = CityDb.SelectById(1);
-CityDb c1DB = new CityDb();
-Citylist csts = c1DB.SelectAll();
-Console.WriteLine("students list before :");
-foreach (City1 c in csts)
-    Console.WriteLine(c.cityname);
-City1 cLast = csts.Last();
-//c1DB.Delete(sLast);
-//c1DB.SaveChanges();
-Console.WriteLine("delete:");
-csts = c1DB.SelectAll();
-foreach (City1 c in csts)
-    Console.WriteLine(c.cityname);
-City1 cy = new City1() { cityname = "haifa" };
-CityDb cyd = new CityDb();
-cyd.Insert(cy);
-cyd.SaveChanges();
-Citylist cities = cyd.SelectAll();
-foreach (City1 c in cities)
-{
-    Console.WriteLine(c.cityname);
-}
-City1 c2 = cities[2];
-c2.cityname = "natnia";
-cyd.Update(c2);
-cyd.SaveChanges();
-Citylist cities1 = cyd.SelectAll();
-foreach (City1 c in cities1)
-{
-    Console.WriteLine(c.cityname);
-}
-#endregion
-#region
-Model1 mc = ModelDB.SelectById(1);
-ModelDB mDB = new ModelDB();
-Modellist ms = mDB.SelectAll();
-Console.WriteLine("students list before :");
-foreach (Model1 c in ms)
-    Console.WriteLine(c.Moedlname);
-Model1 mLast = ms.Last();
-//mDB.Delete(mLast);
+//#endregion
+//#region
+//Seller sc = sellerDb.SelectById(1);
+//sellerDb sDB = new sellerDb();
+//Sellerlist sts = sDB.SelectAll();
+//Console.WriteLine("students list before :");
+//foreach (Seller c in sts)
+//    Console.WriteLine(c.lname);
+//Seller sLast = sts.Last();
+////sDB.Delete(sLast);
+////sDB.SaveChanges();
+//Console.WriteLine("delete:");
+//sts = sDB.SelectAll();
+//foreach (Seller c in sts)
+//    Console.WriteLine(c.lname);
+//#endregion
+//#region
+//City1 c1c = CityDb.SelectById(1);
+//CityDb c1DB = new CityDb();
+//Citylist csts = c1DB.SelectAll();
+//Console.WriteLine("students list before :");
+//foreach (City1 c in csts)
+//    Console.WriteLine(c.cityname);
+//City1 cLast = csts.Last();
+////c1DB.Delete(sLast);
+////c1DB.SaveChanges();
+//Console.WriteLine("delete:");
+//csts = c1DB.SelectAll();
+//foreach (City1 c in csts)
+//    Console.WriteLine(c.cityname);
+//City1 cy = new City1() { cityname = "haifa" };
+//CityDb cyd = new CityDb();
+//cyd.Insert(cy);
+//cyd.SaveChanges();
+//Citylist cities = cyd.SelectAll();
+//foreach (City1 c in cities)
+//{
+//    Console.WriteLine(c.cityname);
+//}
+//City1 c2 = cities[2];
+//c2.cityname = "natnia";
+//cyd.Update(c2);
+//cyd.SaveChanges();
+//Citylist cities1 = cyd.SelectAll();
+//foreach (City1 c in cities1)
+//{
+//    Console.WriteLine(c.cityname);
+//}
+//#endregion
+//#region
+//Model1 mc = ModelDB.SelectById(1);
+//ModelDB mDB = new ModelDB();
+//Modellist ms = mDB.SelectAll();
+//Console.WriteLine("students list before :");
+//foreach (Model1 c in ms)
+//    Console.WriteLine(c.Moedlname);
+//Model1 mLast = ms.Last();
+////mDB.Delete(mLast);
+////mDB.SaveChanges();
+//Console.WriteLine("delete:");
+//ms = mDB.SelectAll();
+//foreach (Model1 c in ms)
+//    Console.WriteLine(c.Moedlname);
+//Model1 m2 = ms[0];
+//m2.Moedlname = "picanto";
+//mDB.Update(m2);
 //mDB.SaveChanges();
-Console.WriteLine("delete:");
-ms = mDB.SelectAll();
-foreach (Model1 c in ms)
-    Console.WriteLine(c.Moedlname);
-Model1 m2 = ms[0];
-m2.Moedlname = "picanto";
-mDB.Update(m2);
-mDB.SaveChanges();
-Modellist ms1 = mDB.SelectAll();
-foreach (Model1 c in ms1)
-{
-    Console.WriteLine(c.Moedlname);
-}
+//Modellist ms1 = mDB.SelectAll();
+//foreach (Model1 c in ms1)
+//{
+//    Console.WriteLine(c.Moedlname);
+//}
 
-#endregion
-#region
-Buyer by = new Buyer() { city = csts[0], fname = "liroi", lname = "toman", tz = 7689543 };
-BuyerDb byd = new BuyerDb();
-BuyerList bs = byd.SelectAll();
-//byd.Insert(by);
+//#endregion
+//#region
+//Buyer by = new Buyer() { city = csts[0], fname = "liroi", lname = "toman", tz = 7689543 };
+//BuyerDb byd = new BuyerDb();
+//BuyerList bs = byd.SelectAll();
+////byd.Insert(by);
+////byd.SaveChanges();
+//BuyerList buyers = byd.SelectAll();
+//foreach (Buyer c in buyers)
+//{
+//    Console.WriteLine(c.fname);
+//}
+//Buyer bu2 = buyers[2];
+//bu2.fname = "natnia";
+//byd.Update(bu2);
 //byd.SaveChanges();
-BuyerList buyers = byd.SelectAll();
-foreach (Buyer c in buyers)
-{
-    Console.WriteLine(c.fname);
-}
-Buyer bu2 = buyers[2];
-bu2.fname = "natnia";
-byd.Update(bu2);
-byd.SaveChanges();
-BuyerList buyers2 = byd.SelectAll();
-foreach (Buyer c in buyers2)
-{
-    Console.WriteLine(c.fname);
-}
+//BuyerList buyers2 = byd.SelectAll();
+//foreach (Buyer c in buyers2)
+//{
+//    Console.WriteLine(c.fname);
+//}
 
 
-#endregion
-#region
+//#endregion
+//#region
 
-Car cay = new Car() { Location = csts[0], Price = 44400, Modelname = ms[0], CarSeller = sts[1], CarBrand = brs[0], Carkind = kinds[0], pic = "yuy", km = 5666 };
-CarDb cad = new CarDb();
-cad.Insert(cay);
-cad.SaveChanges();
-Carlist cars = cad.SelectAll();
-foreach (Car c in cars)
-{
-    //Console.WriteLine(c.Modelname + " " + c.Carkind + " " + c.CarSeller.fname);
-}
-Car cu2 = new Car() { Location = csts[0], Price = 44400, Modelname = ms[0], CarSeller = sts[1], CarBrand = brs[0], Carkind = kinds[0], pic="yuy", km=5666 };
-cu2.Price = 49000;
-cad.Update(cu2);
-cad.SaveChanges();
-Carlist cras = cad.SelectAll();
-foreach (Car c in cras)
-{
-    Console.WriteLine(c.Price);
-}
-Car caLast = cars.Last();
-cad.Delete(mLast);
-cad.SaveChanges();
-Console.WriteLine("delete:");
-cars = cad.SelectAll();
-foreach (Car c in cars)
-    Console.WriteLine(c.Price);
+//Car cay = new Car() { Location = csts[0], Price = 44400, Modelname = ms[0], CarSeller = sts[1], CarBrand = brs[0], Carkind = kinds[0], pic = "yuy", km = 5666 };
+//CarDb cad = new CarDb();
+//cad.Insert(cay);
+//cad.SaveChanges();
+//Carlist cars = cad.SelectAll();
+//foreach (Car c in cars)
+//{
+//    //Console.WriteLine(c.Modelname + " " + c.Carkind + " " + c.CarSeller.fname);
+//}
+//Car cu2 = new Car() { Location = csts[0], Price = 44400, Modelname = ms[0], CarSeller = sts[1], CarBrand = brs[0], Carkind = kinds[0], pic="yuy", km=5666 };
+//cu2.Price = 49000;
+//cad.Update(cu2);
+//cad.SaveChanges();
+//Carlist cras = cad.SelectAll();
+//foreach (Car c in cras)
+//{
+//    Console.WriteLine(c.Price);
+//}
+//Car caLast = cars.Last();
+//cad.Delete(mLast);
+//cad.SaveChanges();
+//Console.WriteLine("delete:");
+//cars = cad.SelectAll();
+//foreach (Car c in cars)
+//    Console.WriteLine(c.Price);
 #endregion
 #region
 Favorite fc = FavoriteDb.SelectById(1);
@@ -210,30 +210,30 @@ Console.WriteLine("students list before :");
 foreach (Favorite c in fcs)
     Console.WriteLine(c.car);
 Favorite fLast = fcs.Last();
-//fdb.Delete(fLast);
-//fdb.SaveChanges();
-Console.WriteLine("delete:");
-fcs = fdb.SelectAll1();
-foreach (Favorite c in fcs)
-    Console.WriteLine(c.car);
-Favorite fy = new Favorite() { };
-FavoriteDb fyd = new FavoriteDb();
-//kyd.Insert(ky);
-//kyd.SaveChanges();
-Fvoritelist favorits = fyd.SelectAll1();
-foreach (Favorite c in favorits)
-{
-    Console.WriteLine(c.car);
-}
-Favorite fu2 = favorits[2];
-fu2.car = cu2;
-fyd.Update(fu2);
-fyd.SaveChanges();
-Fvoritelist favorites = fyd.SelectAll1();
-foreach (Buyer c in buyers2)
-{
-    Console.WriteLine(c.fname);
-}
+fdb.Delete(fLast);
+fdb.SaveChanges();
+//Console.WriteLine("delete:");
+//fcs = fdb.SelectAll1();
+//foreach (Favorite c in fcs)
+//    Console.WriteLine(c.car);
+//Favorite fy = new Favorite() { };
+//FavoriteDb fyd = new FavoriteDb();
+////kyd.Insert(ky);
+////kyd.SaveChanges();
+//Fvoritelist favorits = fyd.SelectAll1();
+//foreach (Favorite c in favorits)
+//{
+//    Console.WriteLine(c.car);
+//}
+//Favorite fu2 = favorits[2];
+//fu2.car = cu2;
+//fyd.Update(fu2);
+//fyd.SaveChanges();
+//Fvoritelist favorites = fyd.SelectAll1();
+//foreach (Buyer c in buyers2)
+//{
+//    Console.WriteLine(c.fname);
+//}
 //Apiservice apiservice = new();
 //Kindlist cities = await apiservice.GetKinds();
 //Console.WriteLine(cities.Count);
